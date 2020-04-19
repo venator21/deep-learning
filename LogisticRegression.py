5dfb813c54cfa6757964ad4add7cc3af74572327
Piotr Brodzik <pbrodzik@mail.com>
2020-04-19 18:03:57 +0200

    init commit

---
+++ LogisticRegression.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+class LogisticRegression:
+    def __init__(self):
+        pass
+
+    def sigmoid(self, x, y):
+        """
+
+
+        :param x: re
+        :param y:
+        :return:
+        """
+        return 0
